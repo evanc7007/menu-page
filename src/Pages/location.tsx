6cd71161fc792a257b356b7dbd75fc6ba2870b4e
evanc7007 <evanjasonchen@gmail.com>
2026-01-19 22:21:44 -0500

    correctly rounded card

--- src/Pages/location.tsx
+++ src/Pages/location.tsx
@@ -8,7 +8,7 @@ function Location(){
                             <iframe
                                 title="Yum Yum No. 8 Location"
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3231.205387837265!2d-84.57223222473851!3d35.91747241696131!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885ddda954b9efa9%3A0xf297517865237681!2sYum%20Yum!5e0!3m2!1sen-US!2sus!4v1768876909055!5m2!1sen-US!2sus"
-                                className="w-100"
+                                className="w-100 rounded-top"
                                 style={{ height: "400px", border: 0 }}
                                 allowFullScreen
                                 loading="lazy"
